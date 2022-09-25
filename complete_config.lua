a9cac3f33e55c8153641b2a2c0c46c11938e2ab1
GrislyMe <grislyme@csie.io>
2022-09-25 19:44:04 +0800

    switch to lua

---
+++ complete_config.lua
@@ -0,0 +1,33 @@
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	mapping = {
+		['<Tab>'] = cmp.mapping.select_next_item(),
+		['<S-Tab>'] = cmp.mapping.select_prev_item(),
+		['<CR>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' },
+		{ name = 'buffer' },
+		{ name = 'path' }
+	}
+})
+
+-- Setup lspconfig.
+local ls = { 'clangd', 'pyright', 'kotlin_language_server'}
+for i, server in ipairs(ls) do
+	require('lspconfig')[server].setup {
+		on_attach = on_attach,
+		capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+	}
+end
+
+vim.opt.completeopt = {"menu", "menuone", "noselect"}
+
--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
-require('plugin')
+require('nvim_tree_setup')
 require('basic')
+require('keybinding')
+require('plugin')
+require('treesitter')
+require('terminal')
+require('complete_config')
+
+vim.cmd("colorscheme gruvbox")
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
