22b27f750607a4e3979846e9f996c0ea4802ba42
William Hsu <grislyme@gmail.com>
2024-11-14 16:34:56 +0800

    fix: some complete issue when using cmdline

--- complete_config.lua
+++ complete_config.lua
@@ -1,11 +1,17 @@
 -- Setup nvim-cmp.
 local cmp = require 'cmp'
-
 cmp.setup({
+	view = {
+		entries = "custom"
+	},
 	snippet = {
 		expand = function(args)
 			require('snippy').expand_snippet(args.body)
 		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
 	mapping = {
 		['<Tab>'] = cmp.mapping.select_next_item(),
@@ -34,14 +40,40 @@ cmp.setup({
 })
 
 cmp.setup.cmdline({ '/', '?' }, {
-	mapping = cmp.mapping.preset.cmdline(),
+	view = {
+		entries = "custom"
+	},
+	mapping = {
+		['<Tab>'] = {
+			c = function(_)
+				if cmp.visible() then
+					if #cmp.get_entries() == 1 then
+						cmp.confirm({ select = true })
+					else
+						cmp.select_next_item()
+					end
+				else
+					cmp.complete()
+					if #cmp.get_entries() == 1 then
+						cmp.confirm({ select = true })
+					end
+				end
+			end,
+		},
+		['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'c' }),
+		["<CR>"] = cmp.mapping({
+			c = cmp.mapping.confirm({ select = true }),
+		}),
+	},
 	sources = {
 		{ name = 'buffer' }
-	}
+	},
 })
 
 cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
+	view = {
+		entries = "native"
+	},
 	sources = cmp.config.sources({
 		{ name = 'path' }
 	}, {
@@ -76,5 +108,9 @@ lspconfig.clangd.setup {
 		"--offset-encoding=utf-16",
 	},
 }
+
+-- lspconfig.rust_analyzer.setup {
+-- 	capabilities = capabilities,
+-- }
 
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
