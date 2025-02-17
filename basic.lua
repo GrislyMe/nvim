fd8bcc0f565fb3cea200dc489d760602caa7dc38
William Hsu <grislyme@gmail.com>
2025-02-17 17:47:30 +0800

    feat: update the auto cmd for 2-space tab

--- basic.lua
+++ basic.lua
@@ -9,17 +9,22 @@ vim.cmd("set colorcolumn=80")
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
 vim.cmd("set scl=yes:1")
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.o.foldlevel = 99
+-- vim.o.foldenable = true
 
 vim.opt.list = true
 
 vim.o.syntax = "on"
 vim.o.encoding = "utf-8"
 vim.o.backspace = "indent,eol,start"
+
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 vim.o.scrolloff = 5
 vim.o.autoindent = true
 vim.o.number = true
@@ -38,6 +43,10 @@ vim.o.autowrite = true
 vim.o.mouse = "a"
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "lua", "javascript", "html", "c", "cpp", "h", "hpp" },
-	command = "setlocal shiftwidth=2 tabstop=2",
+	pattern = { "lua", "javascript", "html", "c", "cpp", "objc" },
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+	end,
 })
